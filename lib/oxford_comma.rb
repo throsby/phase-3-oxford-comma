d219f67458c2de4a436d1b5b10850e1c52f8776b
Throsby Wells <throsbywells@gmail.com>
2022-04-28 16:15:58 -0400

    commit tree fff4f6f63149d4c217985bf7c1ff97e4c2e3874e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    str_return = ""
+    array.each.with_index do |el, index|
+        puts array.length + " " + index
+        if array.length == 0
+            str_return << el
+            return str_return
+        elsif index == array.length
+                str_return << " and "
+                str_return << el
+        else
+            str_return << el
+        end
+    end
+    p str_return
+    return str_return
 end
